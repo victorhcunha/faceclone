7d60c9c4b7d7a47a8738616f3616f627c14f0e96
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2020-11-18 20:20:13 -0300

    inicio visual

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import {FaFacebook, FaSearch, FaAlignJustify} from 'react-icons/fa'
 function App() {
   return (
     <div className="App">
-        <div className="headerleft">
-          <div className="logo-fb"> <FaFacebook/> </div>
-          <div className="search-fb"> <FaSearch/> </div>
-          <div className="menu-fb"> <FaAlignJustify/> </div>
-        </div>
-
-        <div className="headerright">
-          <div className="plus-btn">
-              +
+      <div className="header">
+          <div className="headerleft">
+            <div className="logo-fb"> <FaFacebook/> </div>
+            <div className="search-fb"> <FaSearch/> </div>
+            <div className="menu-fb"> <FaAlignJustify/> </div>
           </div>
+
+          <div className="headerright">
+            <div className="plus-btn">
+                +
+            </div>
+      </div>
         </div>
     </div>
   );
