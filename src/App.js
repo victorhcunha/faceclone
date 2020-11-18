52bbae49746c79f50c18a326f0fd42a6ab7c1a2c
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2020-11-18 20:14:46 -0300

    app inicio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {FaFacebook, FaSearch, FaAlignJustify} from 'react-icons/fa'
 
 function App() {
   return (
     <div className="App">
+        <div className="headerleft">
+          <div className="logo-fb"> <FaFacebook/> </div>
+          <div className="search-fb"> <FaSearch/> </div>
+          <div className="menu-fb"> <FaAlignJustify/> </div>
+        </div>
 
+        <div className="headerright">
+          <div className="plus-btn">
+              +
+          </div>
+        </div>
     </div>
   );
 }
