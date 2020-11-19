e20f2d63633173dafcb1e052002222fd0751b3cc
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2020-11-18 21:28:33 -0300

    penguin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
 import Header from './Header';
-import Imagem from 'http://www.turismo.gov.br/sei/#';
+import penguin from './resources/penguin.png'; 
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <div className="storiescard" style={{backgroundImage:Imagem}}> 
+      <div className="storiescard" style={{backgroundImage:'url(${penguin})'}}> 
           <p>Fulano de Tal</p>
       </div>
-      <div className="storiescard" style={{backgroundImage:Imagem}}> 
+      <div className="storiescard" style={{backgroundImage:'url(${penguin})'}}>
           <p>Fulano de Tal</p>
       </div>
-      <div className="storiescard" style={{backgroundImage:Imagem}}> 
+      <div className="storiescard" style={{backgroundImage:'url(${penguin})'}}>
           <p>Fulano de Tal</p>
       </div>
-      <div className="storiescard" style={{backgroundImage:Imagem}}> 
+      <div className="storiescard" style={{backgroundImage:'url(${penguin})'}}>
           <p>Fulano de Tal</p>
       </div>
-      <div className="storiescard" style={{backgroundImage:Imagem}}> 
+      <div className="storiescard" style={{backgroundImage:'url(${penguin})'}}>
           <p>Fulano de Tal</p>
       </div>
     </div>
