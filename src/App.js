a8e1c8b19e0f9736b94378b131a51dec058c5ee0
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2020-11-19 08:33:12 -0300

    feed icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import './App.css';
 import Header from './Header';
 import Stories from './Stories'
+import windowsxp from './resources/windowsxp.jpg'; 
+
+import {AiFillVideoCamera, AiOutlineFileImage, AiFillMeh} from 'react-icons/ai'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Stories/>
+      <div className="feed">
+        <div className="postfeed">
+          <img src={windowsxp} alt="img"/>
+          <input type="text" placeholder="No que você está pensando?"/>
+        </div>
+
+        <div className="feedicons">
+
+          <div className="iconSingle">
+            <AiFillVideoCamera/>
+            <spam>Video ao vivo</spam>
+          </div>
+
+          <div className="iconSingle img">
+            <AiOutlineFileImage/>
+            <spam>Video ao vivo</spam>
+          </div>
+
+          <div className="iconSingle emoji">
+            <AiFillMeh/>
+            <spam>Video ao vivo</spam>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
