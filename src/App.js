60e7fb403b06a06cebe70ee7c1825c98428e5dcd
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2020-11-19 07:30:36 -0300

    storiescard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import './App.css';
 import Header from './Header';
-import penguin from './resources/penguin.png'; 
+import Stories from './Stories'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <div className="storiescard" style={{backgroundImage:'url(${penguin})'}}> 
-          <p>Fulano de Tal</p>
-      </div>
-      <div className="storiescard" style={{backgroundImage:'url(${penguin})'}}>
-          <p>Fulano de Tal</p>
-      </div>
-      <div className="storiescard" style={{backgroundImage:'url(${penguin})'}}>
-          <p>Fulano de Tal</p>
-      </div>
-      <div className="storiescard" style={{backgroundImage:'url(${penguin})'}}>
-          <p>Fulano de Tal</p>
-      </div>
-      <div className="storiescard" style={{backgroundImage:'url(${penguin})'}}>
-          <p>Fulano de Tal</p>
-      </div>
+      <Stories/>
     </div>
   );
 }
---
+++ src/Stories.js
@@ -0,0 +1,23 @@
+//import penguin from './resources/penguin.png'; 
+
+export default function(){
+    return(
+        <div className="stories">
+        <div className="storiescard" >
+            <p>Fulano de Tal</p>
+        </div>
+        <div className="storiescard" >
+            <p>Fulano de Tal</p>
+        </div>
+        <div className="storiescard" >
+            <p>Fulano de Tal</p>
+        </div>
+        <div className="storiescard" >
+            <p>Fulano de Tal</p>
+        </div>
+        <div className="storiescard" >
+            <p>Fulano de Tal</p>
+        </div>
+      </div>
+    )
+}
