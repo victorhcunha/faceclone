d705707c04f3bdcf4aea3f28d8d36f4d2027e4da
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2020-11-18 21:10:50 -0300

    inicio stories card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 import Header from './Header';
+import Imagem from 'http://www.turismo.gov.br/sei/#';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className="storiescard" style={{backgroundImage:Imagem}}> 
+          <p>Fulano de Tal</p>
+      </div>
+      <div className="storiescard" style={{backgroundImage:Imagem}}> 
+          <p>Fulano de Tal</p>
+      </div>
+      <div className="storiescard" style={{backgroundImage:Imagem}}> 
+          <p>Fulano de Tal</p>
+      </div>
+      <div className="storiescard" style={{backgroundImage:Imagem}}> 
+          <p>Fulano de Tal</p>
+      </div>
+      <div className="storiescard" style={{backgroundImage:Imagem}}> 
+          <p>Fulano de Tal</p>
+      </div>
     </div>
   );
 }
